51c07592dd14785f8062a3cced98937816d355f3
cjjjjc <40121673+cjjjjc@users.noreply.github.com>
2018-06-25 08:48:59 +0800

    Add files via upload

---
+++ buybook.js
@@ -0,0 +1,15 @@
+// pages/buybook/buybook.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {  
+  },
+  /*suo: function (e) {
+    wx.navigateTo({
+      url: '../search/search',
+    })
+  },  
+*/
+})
