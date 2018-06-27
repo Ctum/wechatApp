ad550d7a43c02879a69fb4ed506dc17e5e6fca6e
cjjjjc <40121673+cjjjjc@users.noreply.github.com>
2018-06-27 14:57:14 +0800

    增加上传照片功能
    这里还需要传到后端数据库

--- pages/typeIn/typeIn.js
+++ pages/typeIn/typeIn.js
@@ -11,7 +11,7 @@ Page({
     press: '',
     pubdate: '',
     pages: '',
-    price: 0,
+    price: 34.01,
     isbn: 0
   },
   submit: function (e) {
@@ -62,5 +62,36 @@ Page({
         }
       })
     }
-  }
+  },
+
+  chooseImageTap: function () {
+    let that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#f7982a",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+  },
+
+  chooseWxImage: function (type) {
+    let _this = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res);
+        _this.setData({
+          logo: res.tempFilePaths[0],
+        })
+      },
+    })
+  },
 })
