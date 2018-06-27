243e93409809a1319b853de62e8a20751fe0b81f
Ctum <shuerhy@163.com>
2018-06-27 20:32:30 +0800

    解决电脑上无法输入的问题

--- components/input/input.js
+++ components/input/input.js
@@ -1,71 +1,62 @@
-// components/input.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  externalClasses: ['i-class'],
-  behaviors: ['wx://form-field'],
-  properties: {
-    title: {
-      type: String
+    behaviors: ['wx://form-field'],
+
+    externalClasses: ['i-class'],
+
+    properties: {
+        title: {
+            type: String
+        },
+        // text || textarea || password || number
+        type: {
+            type: String,
+            value: 'text'
+        },
+        disabled: {
+            type: Boolean,
+            value: false
+        },
+        placeholder: {
+            type: String,
+            value: ''
+        },
+        autofocus: {
+            type: Boolean,
+            value: false
+        },
+        mode: {
+            type: String,
+            value: 'normal'
+        },
+        right: {
+            type: Boolean,
+            value: false
+        },
+        error: {
+            type: Boolean,
+            value: false
+        },
+        maxlength: {
+            type: Number,
+            default: -1
+        }
     },
-    type: {
-      type: String,
-      value: 'text'
-    },
-    disabled: {
-      type: Boolean,
-      value: false
-    },
-    placeholder: {
-        type: String,
-        value: ''
-    },
-    autofocus: {
-        type: Boolean,
-        value: false
-    },
-    mode: {
-        type: String,
-        value: 'normal'
-    },
-    right: {
-        type: Boolean,
-        value: false
-    },
-    error: {
-        type: Boolean,
-        value: false
-    },
-    maxlength: {
-        type: Number
+
+    methods: {
+        handleInputChange(event) {
+            const { detail = {} } = event;
+            const { value = '' } = detail;
+            this.setData({ value });
+
+            this.triggerEvent('change', event);
+        },
+
+        handleInputFocus(event) {
+            this.triggerEvent('focus', event);
+        },
+
+        handleInputBlur(event) {
+            this.triggerEvent('blur', event);
+        }
     }
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    handleInputChange(event) {
-      const { detail = {} } = event;
-      const { value = '' } = detail;
-      this.setData({ value });
-      this.triggerEvent('change', event);
-    },
-
-    handleInputFocus(event) {
-        this.triggerEvent('focus', event);
-    },
-
-    handleInputBlur(event) {
-        this.triggerEvent('blur', event);
-    }
-  }
-})
+});
